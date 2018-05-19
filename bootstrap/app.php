965a5a57a98bebc04affe4f21a2b4ddfbddd90d5
Aldo Choque <achoque1400@gmail.com>
2018-05-19 18:34:21 -0500

    psr-4 autoload slim framework 3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,10 +4,32 @@ session_start();
 
 require __DIR__.'/../vendor/autoload.php';
 
+// $user=new \App\Models\User;
+// var_dump($user);
+// die();
+
 $app=new \Slim\App([
     'settings'=>[
         'displayErrorDetails'=>true,
     ]
 ]);
+
+
+$container=$app->getContainer();
+
+$container['view']=function($container){
+
+    $view=new \Slim\Views\Twig(__DIR__.'/../resources/views',[
+        'cache'=>false,
+    ]);
+
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $container->router,
+        $container->request->getUri()
+    ));
+
+    return $view;
+};
+
 
 require __DIR__.'/../app/routes.php';
