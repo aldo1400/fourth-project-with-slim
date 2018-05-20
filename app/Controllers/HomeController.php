58f633aa90e754f5aaee28a307440180bc586404
Aldo Choque <achoque1400@gmail.com>
2018-05-19 22:10:28 -0500

    Create user with Eloquent ORM

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 namespace App\Controllers;
 // Esta linea no es necesaria por el uso de Base COntroller
 use Slim\Views\Twig as View;
+use App\Models\User; 
 
 class HomeController extends Controller
 {
@@ -10,9 +11,11 @@ class HomeController extends Controller
     public function index($request,$response){
 
     //    var_dump($request->getParam('name')); Obtener parametros de un get  ?name
-    $user=$this->db->table('users')->find(1);
-    var_dump($user->email);
-    die();
+    User::create([
+        'name'=>'Alex Garret',
+        'email'=>'billy@codecourse',
+        'password'=>'123',
+    ]);
     return $this->view->render($response,'home.twig');
     }
 
